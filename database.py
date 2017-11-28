e837ce2e7c3ddced403906daeed9e391d0cfb7d0
Emily Zhang <jingwenz@udel.edu>
2017-11-27 19:42:56 -0500

    first edit

---
+++ database.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SECRET KEY'] = "33445566789"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/members')
+def member():
+    return render_template('members.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
